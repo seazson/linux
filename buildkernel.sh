0309cdf8a8864937fee667fba1483e857df4cef9
seazson <seazson@163.com>
2016-11-08 16:56:09 +0800

    add rootfs.tar.bz2

--- buildkernel.sh
+++ buildkernel.sh
@@ -1,7 +1,14 @@
 #! /bin/bash
 
 echo "building kernel...................."
-cp arch/arm/configs/z2440_defconfig .config
+if [ ! -d ../rootfs ]; then
+	tar xvjf rootfs.tar.bz2 -C ../ 
+fi
+
+if [ ! -f ".config" ]; then
+	cp arch/arm/configs/z2440_defconfig .config
+fi
+
 make uImage
 
 echo "building done!"
